a5091ae773a27102303b1d3d9f8be9aad4075ff3
David Zukowski <zukoweb@gmail.com>
2015-08-08 09:39:58 -0400

    update(react-redux): bumps version to ^0.5.0 and makes necessary API changes to Provider and devTools to accomodate

--- src/containers/app/index.jsx
+++ src/containers/app/index.jsx
@@ -1,13 +1,11 @@
 /* eslint-disable */
 import React from 'react';
-import { provide } from 'react-redux';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import store from 'stores';
 import routes from 'routes';
-import { DevTools, LogMonitor, DebugPanel } from 'redux-devtools/lib/react';
 /* eslint-enable */
 
-@provide(store)
 export default class ClientApp extends React.Component {
   static propTypes = {
     history      : React.PropTypes.object,
@@ -20,16 +18,6 @@ export default class ClientApp extends React.Component {
 
   constructor () {
     super();
-  }
-
-  renderDevTools () {
-    if (__DEBUG__) {
-      return (
-        <DebugPanel top left bottom key='debugPanel'>
-          <DevTools store={store} monitor={LogMonitor} />
-        </DebugPanel>
-      );
-    }
   }
 
   renderRouter () {
@@ -52,10 +40,9 @@ export default class ClientApp extends React.Component {
 
   render () {
     return (
-      <div>
-        {this.renderDevTools()}
-        {this.renderRouter()}
-      </div>
+      <Provider store={store}>
+        {() => this.renderRouter()}
+      </Provider>
     );
   }
 }
--- src/layouts/core/index.jsx
+++ src/layouts/core/index.jsx
@@ -1,4 +1,6 @@
 import React  from 'react';
+import store from 'stores';
+import { DevTools, LogMonitor, DebugPanel } from 'redux-devtools/lib/react';
 import './core-layout.scss';
 
 export default class CoreLayout extends React.Component {
@@ -6,9 +8,18 @@ export default class CoreLayout extends React.Component {
     super();
   }
 
+  renderDevTools () {
+    return (
+      <DebugPanel top left bottom key='debugPanel'>
+        <DevTools store={store} monitor={LogMonitor} />
+      </DebugPanel>
+    );
+  }
+
   render () {
     return (
       <div className='page-container'>
+        {__DEBUG__  && this.renderDevTools()}
         <div className='view-container'>
           {this.props.children}
         </div>
