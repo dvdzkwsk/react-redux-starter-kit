51d7c262d65ef34a7b09b1d473a3c01720fed9c5
David Zukowski <zukoweb@gmail.com>
2015-08-08 11:22:38 -0400

    refactor(dev-tools): moves redux dev-tools back into app container rather than core layout. Layout should remain pure

--- src/layouts/core/index.jsx
+++ src/layouts/core/index.jsx
@@ -1,6 +1,4 @@
 import React  from 'react';
-import store from 'stores';
-import { DevTools, LogMonitor, DebugPanel } from 'redux-devtools/lib/react';
 import './core-layout.scss';
 
 export default class CoreLayout extends React.Component {
@@ -8,18 +6,9 @@ export default class CoreLayout extends React.Component {
     super();
   }
 
-  renderDevTools () {
-    return (
-      <DebugPanel top left bottom key='debugPanel'>
-        <DevTools store={store} monitor={LogMonitor} />
-      </DebugPanel>
-    );
-  }
-
   render () {
     return (
       <div className='page-container'>
-        {__DEBUG__  && this.renderDevTools()}
         <div className='view-container'>
           {this.props.children}
         </div>
