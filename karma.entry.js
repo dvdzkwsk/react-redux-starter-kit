a91137eedffe69050d034653ce3583c206332411
Roman Boiko <rb@clearjs.com>
2015-08-09 22:25:21 +0300

    fix(regex): typos

--- karma.entry.js
+++ karma.entry.js
@@ -1,4 +1,4 @@
 // Require all ".spec.js" files in ~/src.
-var context = require.context('./src', true, /.+\.spec\.js?$/);
+var context = require.context('./src', true, /.+\.spec\.js$/);
 context.keys().forEach(context);
 module.exports = context;
