b58772eab5ea2e4563615566816461b36eb1cc22
David Zukowski <zukoweb@gmail.com>
2015-08-09 20:06:38 -0400

    fix(koa): cleans up dummy data used for testing rendering

--- server/middleware/render-route.js
+++ server/middleware/render-route.js
@@ -7,7 +7,7 @@ function renderIntoTemplate (template, content) {
 
 module.exports = function makeRenderRouteMiddleware (template) {
   return function *renderRouteMiddleware () {
-    const rendered = yield router('asdasd');
+    const rendered = yield router(this.request);
 
     this.body = renderIntoTemplate(template, rendered);
   };
