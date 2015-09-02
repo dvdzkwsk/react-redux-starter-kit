8ebe8f61c89f32615d0174070c44e344a4424bf5
David Zukowski <zukoweb@gmail.com>
2015-09-02 12:14:57 -0400

    feat(example): updates home view and sample reducer to be more intuitive

--- client/views/home.jsx
+++ client/views/home.jsx
@@ -2,27 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 @connect(state => ({
-  sampleStore : state.sample
+  counter : state.counter
 }))
 export default class HomeView extends React.Component {
   constructor () {
     super();
   }
 
-  _sampleAction () {
+  _increment () {
 
     // normally you'd import an action creator, but I don't want to create
     // a file that you're just going to delete anyways!
-    this.props.dispatch({ type : 'SAMPLE_ACTION' });
+    this.props.dispatch({ type : 'COUNTER_INCREMENT' });
   }
 
   render () {
     return (
-      <div className='view view--home container'>
-        <h1 className='text-center'
-            onClick={::this._sampleAction}>
-            {this.props.sampleStore.message}
-        </h1>
+      <div className='container'>
+        <h1>Welcome to the React Redux Starter Kit</h1>
+        <h2>Sample Counter: {this.props.counter}</h2>
+        <button onClick={::this._increment}>Increment</button>
       </div>
     );
   }
