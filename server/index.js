932085718aa086fb0eb463366f255ce866c63713
David Zukowski <zukoweb@gmail.com>
2015-09-07 12:05:36 -0400

    feat(server): adds ability to inject initial state into application from server

--- server/index.js
+++ server/index.js
@@ -23,8 +23,17 @@ app.use(serve(paths.dist('client'), {
 // ------------------------------------
 // View Rendering
 // ------------------------------------
+// TODO: there's a cleaner way to do this. The reason we're using the
+// compiled .html file is so that we don't have to worry about query strings
+// on generated assets.
 const template = fs.readFileSync(paths.dist('client/index.html'), 'utf-8')
-  .replace('<div id="root"></div>', '<div id="root">${content}</div>');
+  .replace(
+    '<div id="root"></div>',
+    [
+      '<div id="root">${content}</div>',
+      '<script>window.__INITIAL_STATE__ = ${initialState}</script>'
+    ].join('')
+  );
 
 app.use(require('./middleware/render-route')(template));
 
--- server/middleware/render-route.js
+++ server/middleware/render-route.js
@@ -1,15 +1,20 @@
-import runRouter from '../../dist/server';
+import { render, route, getStoreState } from '../../dist/server';
 
-function renderIntoTemplate (template, content) {
-  return template.replace('${content}', content);
+// TODO: should probably use a tagged template
+function renderIntoTemplate (template, content, initialState) {
+  return template
+    .replace('${content}', content)
+    .replace('${initialState}', JSON.stringify(initialState));
 }
 
 export default function makeRenderRouteMiddleware (template) {
   return function *renderRouteMiddleware (next) {
     try {
-      const rendered = yield runRouter(this.request);
+      const routerState = yield route(this.request);
+      const rendered    = yield render(routerState, { counter : 5 });
+      const storeState  = yield getStoreState();
 
-      this.body = renderIntoTemplate(template, rendered);
+      this.body = renderIntoTemplate(template, rendered, storeState);
     } catch (e) {
       console.log(e);
       yield next;
