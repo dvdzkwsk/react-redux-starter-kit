b595b6b6cc892599426970c0288599151e394b49
David Zukowski <zukoweb@gmail.com>
2015-09-15 10:32:58 -0400

    clean(counter): remove unnecessary braces in incremenent handler

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,14 +1,10 @@
 import { createReducer } from 'utils';
 
 // normally this would be imported from /constants, but in trying to keep
-// this starter kit as easy to customize as possibility we'll just define
-// the constant here.
+// this starter kit as small as possible we'll just define it here.
 const COUNTER_INCREMENT = 'COUNTER_INCREMENT';
 
 const initialState = 0;
-
 export default createReducer(initialState, {
-  [COUNTER_INCREMENT] : (state) => {
-    return state + 1;
-  }
+  [COUNTER_INCREMENT] : (state) => state + 1
 });
