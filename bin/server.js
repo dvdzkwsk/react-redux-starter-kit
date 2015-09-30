e248f8ef973f83e78e76e67413a6ff68b25f4059
David Zukowski <zukoweb@gmail.com>
2015-09-30 10:52:37 -0400

    fix(server): remove unused bin/server and Procfile

--- bin/server.js
+++
@@ -1,9 +0,0 @@
-require('babel/register');
-
-const server = require('../server'),
-      config = require('../config');
-
-const port = config.get('server_port');
-
-server.listen(port);
-console.log('Koa server listening on port: ' + port);
