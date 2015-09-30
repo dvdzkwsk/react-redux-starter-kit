9297b52ac896f3824093d87d1be8ad633a8650b1
David Zukowski <zukoweb@gmail.com>
2015-09-30 11:11:45 -0400

    fix(webpack): directly export config object, rather than array

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 require('babel/register');
 
 const config   = require('./config');
-module.exports = exports = [
-  require('./build/webpack/' + config.get('env'))
-];
+module.exports = require('./build/webpack/' + config.get('env'));
