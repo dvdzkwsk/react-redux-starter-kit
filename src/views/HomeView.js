a2428ccaecc1fc7d28f350d3f633addc06699c1f
David Zukowski <zukoweb@gmail.com>
2015-10-13 15:23:22 -0400

    refactor(HomeView): use mapDispatchToProps to better show the "redux way"

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,28 +1,32 @@
-import React       from 'react';
-import { connect } from 'react-redux';
+import React                  from 'react';
+import { bindActionCreators } from 'redux';
+import { connect }            from 'react-redux';
 
-// We define mapStateToProps where we'd normally use the @connect
-// decorator so the data requirements are clear upfront, but then
+// Normally you'd import your action creators, but I don't want to create
+// a file that you're just going to delete anyways!
+const actionCreators = {
+  increment : () => ({ type : 'COUNTER_INCREMENT' })
+};
+
+// We define mapStateToProps and mapDispatchToProps where we'd normally use
+// the @connect decorator so the data requirements are clear upfront, but then
 // export the decorated component after the main class definition so
 // the component can be tested w/ and w/o being connected.
 // See: http://rackt.github.io/redux/docs/recipes/WritingTests.html
 const mapStateToProps = (state) => ({
   counter : state.counter
 });
+const mapDispatchToProps = (dispatch) => ({
+  actions : bindActionCreators(actionCreators, dispatch)
+});
 export class HomeView extends React.Component {
   static propTypes = {
-    dispatch : React.PropTypes.func,
+    actions  : React.PropTypes.object,
     counter  : React.PropTypes.number
   }
 
   constructor () {
     super();
-  }
-
-  // normally you'd import an action creator, but I don't want to create
-  // a file that you're just going to delete anyways!
-  _increment () {
-    this.props.dispatch({ type : 'COUNTER_INCREMENT' });
   }
 
   render () {
@@ -31,7 +35,7 @@ export class HomeView extends React.Component {
         <h1>Welcome to the React Redux Starter Kit</h1>
         <h2>Sample Counter: {this.props.counter}</h2>
         <button className='btn btn-default'
-                onClick={::this._increment}>
+                onClick={this.props.actions.increment}>
           Increment
         </button>
       </div>
@@ -39,4 +43,4 @@ export class HomeView extends React.Component {
   }
 }
 
-export default connect(mapStateToProps)(HomeView);
+export default connect(mapStateToProps, mapDispatchToProps)(HomeView);
