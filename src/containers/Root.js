b5b58b851d7ff84980e7d8cd457883404fd584f3
David Zukowski <zukoweb@gmail.com>
2015-10-27 09:39:33 -0400

    feat(devtools): wrap standalone devtools in a provider

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,13 +1,29 @@
-import React           from 'react';
-import { Provider }    from 'react-redux';
-import routes          from '../routes';
-import { ReduxRouter } from 'redux-router';
-import DevTools        from './DevTools';
+import React                    from 'react';
+import { Provider }             from 'react-redux';
+import routes                   from '../routes';
+import { ReduxRouter }          from 'redux-router';
+import DevTools                 from './DevTools';
+import { createDevToolsWindow } from '../utils';
 
 export default class Root extends React.Component {
   static propTypes = {
     store : React.PropTypes.object.isRequired,
-    debug : React.PropTypes.bool
+    debug : React.PropTypes.bool,
+    debugExternal : React.PropTypes.bool
+  }
+
+  static defaultProps = {
+    debug : false,
+    debugExternal : false
+  }
+
+  renderDevTools () {
+    if (!this.props.debug) {
+      return null;
+    }
+
+    return this.props.debugExternal ?
+      createDevToolsWindow(this.props.store) : <DevTools />;
   }
 
   render () {
@@ -18,7 +34,7 @@ export default class Root extends React.Component {
             <ReduxRouter>
               {routes}
             </ReduxRouter>
-            {this.props.debug && <DevTools />}
+            {this.renderDevTools()}
           </div>
         </Provider>
       </div>
