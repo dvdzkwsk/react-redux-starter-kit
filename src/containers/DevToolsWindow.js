7db0e51d11e8fb649af572b86f548030733aa999
Jake <me@jh.gg>
2015-10-28 16:01:22 -0400

    Better handling of `dev:nw`, we don't need to use the dock component if we're using the new window for the dev tools.
    Additionally, configure the new window to open at a more sane size.

---
+++ src/containers/DevToolsWindow.js
@@ -0,0 +1,7 @@
+import React              from 'react';
+import { createDevTools } from 'redux-devtools';
+import LogMonitor         from 'redux-devtools-log-monitor';
+
+export default createDevTools(
+  <LogMonitor />
+);
