b2d6baec715c8e481eaab55c3c92ec1260165a39
David Zukowski <zukoweb@gmail.com>
2015-11-10 13:47:37 -0500

    test(CoreLayout): add basic rendering tests to make sure style imports dont break tests

---
+++ tests/layouts/CoreLayout.spec.js
@@ -0,0 +1,44 @@
+import React      from 'react';
+import TestUtils  from 'react-addons-test-utils';
+import CoreLayout from 'layouts/CoreLayout';
+
+function shallowRender (component) {
+  const renderer = TestUtils.createRenderer();
+
+  renderer.render(component);
+  return renderer.getRenderOutput();
+}
+
+function renderWithProps (props = {}) {
+  return TestUtils.renderIntoDocument(<CoreLayout {...props} />);
+}
+
+function shallowRenderWithProps (props = {}) {
+  return shallowRender(<CoreLayout {...props} />);
+}
+
+describe('(Layout) Core', function () {
+  let _component, _rendered, _props, _child;
+
+  beforeEach(function () {
+    _child = <h1 className='child'>Child</h1>;
+    _props = {
+      children : _child
+    };
+
+    _component = shallowRenderWithProps(_props);
+    _rendered  = renderWithProps(_props);
+  });
+
+  it('Should render as a <div>.', function () {
+    expect(_component.type).to.equal('div');
+  });
+
+  it('Should render a child component.', function () {
+    const child = TestUtils.findRenderedDOMComponentWithClass(
+      _rendered, 'child'
+    );
+
+    expect(child).to.exist;
+  });
+});
