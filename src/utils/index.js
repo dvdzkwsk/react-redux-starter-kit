db01af3351e961e85ba064206f18a2768b663638
Justin Greenberg <me@justingreenberg.com>
2015-12-10 02:31:27 -0500

    fix(babel): remove ...rest from createReducer
    [breaking]

--- src/utils/index.js
+++ src/utils/index.js
@@ -12,10 +12,10 @@ export function createConstants (...constants) {
 
 export function createReducer (initialState, fnMap) {
   // possible use of rest arguments: https://github.com/rackt/redux/issues/749#issuecomment-141570236
-  return (state = initialState, { type, payload }, ...rest) => {
+  return (state = initialState, { type, payload }) => {
     const handler = fnMap[type];
 
-    return handler ? handler(state, payload, ...rest) : state;
+    return handler ? handler(state, payload) : state;
   };
 }
 
