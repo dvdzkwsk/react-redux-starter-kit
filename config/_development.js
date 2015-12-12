64efccc919275b6d82dc83bf8b72df2a7e106d32
David Zukowski <zukoweb@gmail.com>
2015-12-12 10:46:48 -0500

    fix(config): only override publicPath when served by webpack

--- config/_development.js
+++ config/_development.js
@@ -1,9 +1,19 @@
 import { argv } from 'yargs';
 
-export default (config) => ({
-  compiler_enable_hmr  : !!argv.hot,
+export default (config) => {
+  const HMR_ENABLED = !!argv.hot;
+  const overrides = {
+    compiler_enable_hmr : HMR_ENABLED
+  };
 
-  // We use an explicit public path in development to resolve this issue:
+  // We use an explicit public path when the assets are served by webpack
+  // to fix this issue:
   // http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809
-  compiler_public_path : `http://${config.server_host}:${config.server_port}/`
-});
+  if (HMR_ENABLED) {
+    overrides.compiler_public_path = (
+      `http://${config.server_host}:${config.server_port}/`
+    );
+  }
+
+  return overrides;
+};
