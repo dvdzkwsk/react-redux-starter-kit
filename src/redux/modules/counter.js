ddd0aebeee85959dcda6888bdffe36c20be0349b
David Zukowski <zukoweb@gmail.com>
2015-12-13 18:58:13 -0500

    feat(counter): add paylod with value property to denote amount to add

--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -8,7 +8,13 @@ const COUNTER_INCREMENT = 'COUNTER_INCREMENT'
 // ------------------------------------
 // Actions
 // ------------------------------------
-export const increment = () => ({ type: COUNTER_INCREMENT })
+export const increment = () => ({
+  type: COUNTER_INCREMENT,
+  payload: {
+    value: 1
+  }
+})
+
 export const actions = {
   increment
 }
@@ -17,5 +23,5 @@ export const actions = {
 // Reducer
 // ------------------------------------
 export default createReducer(0, {
-  [COUNTER_INCREMENT]: (state) => state + 1
+  [COUNTER_INCREMENT]: (state, { value }) => state + value
 })
