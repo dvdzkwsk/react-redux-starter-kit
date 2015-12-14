97032274dcf1e93e14847e7ba262bd33befcd988
Justin Greenberg <me@justingreenberg.com>
2015-12-13 19:48:20 -0500

    refactor(AboutView): switch to component class

--- src/views/AboutView.js
+++ src/views/AboutView.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-const AboutView = () => (
-  <div className='container text-center'>
-    <h1>This is the about view!</h1>
-    <hr />
-    <Link to='/'>Back To Home View</Link>
-  </div>
-)
+export class AboutView extends React.Component {
+  render () {
+    return (
+      <div className='container text-center'>
+        <h1>This is the about view!</h1>
+        <hr />
+        <Link to='/'>Back To Home View</Link>
+      </div>
+    )
+  }
+}
 
 export default AboutView
