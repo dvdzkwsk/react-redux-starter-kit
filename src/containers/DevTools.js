a8da119a53485a558958c14cee5f258b858eb904
David Zukowski <zukoweb@gmail.com>
2015-12-15 18:50:54 -0500

    chore(DevTools): use default theme

--- src/containers/DevTools.js
+++ src/containers/DevTools.js
@@ -7,6 +7,6 @@ export default createDevTools(
   <DockMonitor
     toggleVisibilityKey='ctrl-h'
     changePositionKey='ctrl-q' >
-    <LogMonitor theme='twilight' />
+    <LogMonitor />
   </DockMonitor>
 )
