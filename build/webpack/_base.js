bfcf5617a4855b3058b0b56155bc33cef5128905
David Zukowski <zukoweb@gmail.com>
2015-12-17 11:44:17 -0500

    feat(json-loader): add dependency and configure webpack

--- build/webpack/_base.js
+++ build/webpack/_base.js
@@ -82,6 +82,10 @@ const webpackConfig = {
         }
       },
       {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
         test: /\.scss$/,
         loaders: [
           'style',
