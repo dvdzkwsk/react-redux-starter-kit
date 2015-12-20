ac97e7050abe33a1f28f707e2f8a356a24a27c1c
David Zukowski <zukoweb@gmail.com>
2015-12-20 10:24:13 -0500

    feat(compile): copy static assets to dist folder

--- bin/compile.js
+++ bin/compile.js
@@ -2,9 +2,11 @@ require('babel-register')
 
 const config = require('../config')
 const debug = require('debug')('app:bin:compile')
+const fs = require('fs-extra')
+
+const paths = config.utils_paths
 
 debug('Create webpack compiler.')
-
 const compiler = require('webpack')(require('../build/webpack'))
 
 compiler.run(function (err, stats) {
@@ -29,4 +31,7 @@ compiler.run(function (err, stats) {
   } else {
     debug('No errors or warnings encountered.')
   }
+
+  debug('Copy static assets to dist folder.')
+  fs.copySync(paths.client('assets'), paths.dist())
 })
