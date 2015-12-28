a1ff6468a8502fca758ba0c367b550a731c12304
David Zukowski <zukoweb@gmail.com>
2015-12-27 21:42:44 -0500

    feat(react-hmre): replace hmr transforms with preset

--- build/webpack-environments/development.js
+++ build/webpack-environments/development.js
@@ -36,49 +36,12 @@ export default (webpackConfig) => {
     // configuration will break other tasks such as test:unit because Webpack
     // HMR is not enabled there, and these transforms require it.
     webpackConfig.module.loaders = webpackConfig.module.loaders.map(loader => {
-      if (/babel/.test(loader.loader)) {
-        debug('Apply react-transform-hmr to babel development transforms')
-
-        // Check if this fork has implemented their own development environment
-        // transforms in _base and log a warning so that any unexpected
-        // behavior is documented.
-        if (
-          loader.query.env &&
-          loader.query.env.development &&
-          loader.query.env.development.plugins
-        ) {
-          debug([
-            'It looks like you\'ve added development-specific transforms to',
-            'babel-loader via the "env" property. This configuration is',
-            'modified in "`~/build/webpack-environments/development" in',
-            'order to apply redbox-react and react-transform-hmr, so you',
-            'may have to adjust that logic to account for your added',
-            'transforms.'
-          ].join(' '))
-        } else {
-          // this path didn't exist, so create all non-existent objects.
-          loader.query.env = loader.query.env || {}
-          loader.query.env.development = loader.query.env.development || {}
-        }
-
-        // Yeah... the new Babel plugin configuration is _really_ ugly. :/
-        // They're essentially tuples, with [pluginName, pluginConfig], so
-        // we push our new tuple into the collection of existing transforms.
-        const plugins = loader.query.env.development.plugins || []
-        loader.query.env.development.plugins = [...plugins, [
-          'react-transform', {
-            transforms: [
-              {
-                transform: 'react-transform-catch-errors',
-                imports: ['react', 'redbox-react']
-              }, {
-                transform: 'react-transform-hmr',
-                imports: ['react'],
-                locals: ['module']
-              }
-            ]
-          }
-        ]]
+      if (
+        /babel/.test(loader.loader) &&
+        !~loader.query.presets.indexOf('react-hmre')
+      ) {
+        debug('Apply react-transform-hmre preset.')
+        loader.query.presets.push('react-hmre')
       }
 
       return loader
