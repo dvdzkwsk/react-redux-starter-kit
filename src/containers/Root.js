89914044955ad1286e87f73943110ea3d831ab7f
Arnaud Rebts <arnaud.rebts@gmail.com>
2016-01-07 14:45:06 +0100

    style(eslint): fix eslint warnings

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -6,7 +6,7 @@ export default class Root extends React.Component {
     history: React.PropTypes.object.isRequired,
     routes: React.PropTypes.element.isRequired,
     store: React.PropTypes.object.isRequired
-  }
+  };
 
   get content () {
     return (
