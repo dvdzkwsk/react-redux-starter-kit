afda57445178721a3a94dfaa5c75c91c8886c456
David Zukowski <zukoweb@gmail.com>
2016-01-07 16:36:32 -0500

    hotfix(babel): add semi colons to class props for babel 6.4.2

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -16,7 +16,7 @@ export class HomeView extends React.Component {
     counter: React.PropTypes.number.isRequired,
     doubleAsync: React.PropTypes.func.isRequired,
     increment: React.PropTypes.func.isRequired
-  }
+  };
 
   render () {
     return (
