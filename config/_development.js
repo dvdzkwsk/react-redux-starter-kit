ebfd115b0360a69d61d0e6de43efbbc4775c4a61
derek <yizi.zhou@gmail.com>
2016-01-20 00:44:26 -0800

    added koa proxy

--- config/_development.js
+++ config/_development.js
@@ -2,5 +2,13 @@
 // to fix this issue:
 // http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809
 export default (config) => ({
-  compiler_public_path: `http://${config.server_host}:${config.server_port}/`
+  compiler_public_path: `http://${config.server_host}:${config.server_port}/`,
+  proxy: {
+    enabled: false,
+    options: {
+      // koa-proxy options
+      host: 'http://localhost:8000',
+      match: /^\/api\/.*/
+    }
+  }
 })
