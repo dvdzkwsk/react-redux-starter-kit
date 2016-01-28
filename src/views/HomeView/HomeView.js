870fc0f21bc9f79fa8b0f1843075e1abe73efacb
Vineet Hawal <vineethawal1989@gmail.com>
2016-01-28 11:50:32 +0530

    remove unused vars

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
 import { actions as counterActions } from '../../redux/modules/counter'
