6e5b613adb727c8757972f1145ee55bb32887a93
Ilya Panasenko <hottdogg2008@gmail.com>
2016-01-28 18:44:50 +0200

    fix(build): redux-devtools dead code removal

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,20 +3,18 @@ import { syncHistory } from 'react-router-redux'
 import thunk from 'redux-thunk'
 import rootReducer from './rootReducer'
 
-function withDevTools (middleware) {
-  const devTools = window.devToolsExtension
-    ? window.devToolsExtension()
-    : require('containers/DevTools').default.instrument()
-  return compose(middleware, devTools)
-}
-
 export default function configureStore ({ initialState = {}, history }) {
   // Sync with router via history instance (main.js)
   const routerMiddleware = syncHistory(history)
 
   // Compose final middleware and use devtools in debug environment
   let middleware = applyMiddleware(thunk, routerMiddleware)
-  if (__DEBUG__) middleware = withDevTools(middleware)
+  if (__DEBUG__) {
+    const devTools = window.devToolsExtension
+      ? window.devToolsExtension()
+      : require('containers/DevTools').default.instrument()
+    middleware = compose(middleware, devTools)
+  }
 
   // Create final store and subscribe router in debug env ie. for devtools
   const store = middleware(createStore)(rootReducer, initialState)
