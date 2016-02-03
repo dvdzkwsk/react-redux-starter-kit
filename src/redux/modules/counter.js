b84f30802eed70308178db212882ccd53a77eb98
David Zukowski <zukoweb@gmail.com>
2016-02-03 16:35:59 -0500

    refactor(counter): do not use redux-actions

--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -1,5 +1,4 @@
-import { createAction, handleActions } from 'redux-actions'
-
+/* @flow */
 // ------------------------------------
 // Constants
 // ------------------------------------
@@ -8,7 +7,10 @@ export const COUNTER_INCREMENT = 'COUNTER_INCREMENT'
 // ------------------------------------
 // Actions
 // ------------------------------------
-export const increment = createAction(COUNTER_INCREMENT, (value = 1) => value)
+export const increment = (value: number = 1): Action => ({
+  type: COUNTER_INCREMENT,
+  payload: value,
+})
 
 // This is a thunk, meaning it is a function that immediately
 // returns a function for lazy evaluation. It is incredibly useful for
@@ -30,8 +32,18 @@ export const actions = {
 }
 
 // ------------------------------------
+// Action Handlers
+// ------------------------------------
+const ACTION_HANDLERS = {
+  [COUNTER_INCREMENT]: (state, {payload}) => state + payload
+}
+
+// ------------------------------------
 // Reducer
 // ------------------------------------
-export default handleActions({
-  [COUNTER_INCREMENT]: (state, { payload }) => state + payload
-}, 1)
+const initialState = 0
+export default function counterReducer (state: number = initialState, action: Action): Object {
+  const handler = ACTION_HANDLERS[action.type]
+
+  return handler ? handler(state, action) : state
+}
