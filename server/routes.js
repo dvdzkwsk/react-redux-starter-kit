1212d013a12b6b5c78b7e1391f1e163f7762d614
Christian Sakai <christianmsakai@gmail.com>
2016-02-13 01:13:53 -0500

    added routes

---
+++ server/routes.js
@@ -0,0 +1,26 @@
+/**
+ * Main application routes
+ */
+
+'use strict';
+
+import errors from './components/errors';
+import path from 'path';
+
+export default function(app) {
+  // Insert routes below
+  app.use('/api/things', require('./api/thing'));
+  app.use('/api/users', require('./api/user'));
+
+  app.use('/auth', require('./auth'));
+
+  // All undefined asset or api routes should return a 404
+  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+   .get(errors[404]);
+
+  // All other routes should redirect to the index.html
+  app.route('/*')
+    .get((req, res) => {
+      res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
+    });
+}
