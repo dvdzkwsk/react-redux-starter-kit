0bbad56dd7d425151d70f1769bde6b5b7128fb00
David Zukowski <zukoweb@gmail.com>
2016-02-14 12:20:27 -0500

    chore(HomeView): add flow typings

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -5,15 +5,26 @@ import { actions as counterActions } from '../../redux/modules/counter'
 import DuckImage from './Duck.jpg'
 import classes from './HomeView.scss'
 
+// We can use Flow (http://flowtype.org/) to type our component's props
+// and state. For convenience we've included both regular propTypes and
+// Flow types, but if you want to try just using Flow you'll want to
+// disable the eslint rule `react/prop-types`.
+// NOTE: You can run `npm run flow:check` to check for any errors in your
+// code, or `npm i -g flow-bin` to have access to the binary globally.
+// Sorry Windows users :(.
+type Props = {
+  counter: number,
+  doubleAsync: Function,
+  increment: Function
+};
+
 // We define mapStateToProps where we'd normally use
 // the @connect decorator so the data requirements are clear upfront, but then
 // export the decorated component after the main class definition so
 // the component can be tested w/ and w/o being connected.
 // See: http://rackt.github.io/redux/docs/recipes/WritingTests.html
-const mapStateToProps = (state) => ({
-  counter: state.counter
-})
-export class HomeView extends React.Component {
+
+export class HomeView extends React.Component<void, Props, void> {
   _handleIncrement: Function;
 
   static propTypes = {
@@ -59,4 +70,7 @@ export class HomeView extends React.Component {
   }
 }
 
-export default connect(mapStateToProps, counterActions)(HomeView)
+const mapStateToProps = (state) => ({
+  counter: state.counter
+})
+export default connect((mapStateToProps), counterActions)(HomeView)
