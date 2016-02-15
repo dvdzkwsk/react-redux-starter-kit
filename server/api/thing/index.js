51b02bb60bbb5921154dca0792969619713b4441
Christian Sakai <christianmsakai@gmail.com>
2016-02-14 23:55:31 -0500

    CRUD successful for thing

--- server/api/thing/index.js
+++ server/api/thing/index.js
@@ -4,10 +4,10 @@ import * as controller from './thing.controller';
 const router = new Router();
 
 router.get('/', controller.index);
-// router.get('/:id', controller.show);
-// router.post('/', controller.create);
-// router.put('/:id', controller.update);
-// router.patch('/:id', controller.update);
-// router.delete('/:id', controller.destroy);
+router.get('/:id', controller.show);
+router.post('/', controller.create);
+router.put('/:id', controller.update);
+router.patch('/:id', controller.update);
+router.delete('/:id', controller.destroy);
 
 export default router;
