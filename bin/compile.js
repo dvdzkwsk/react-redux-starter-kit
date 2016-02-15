f1513a9a6bb7a5fad9c7d8386d4780beb3c5807f
David Zukowski <zukoweb@gmail.com>
2016-02-15 12:29:52 -0500

    refactor(compile): make webpack compiler reusable

--- bin/compile.js
+++ bin/compile.js
@@ -1,38 +1,23 @@
 import fs from 'fs-extra'
 import _debug from 'debug'
-import webpack from 'webpack'
+import webpackCompiler from '../build/webpack-compiler'
 import webpackConfig from '../build/webpack.config'
 import config from '../config'
 
 const debug = _debug('app:bin:compile')
 const paths = config.utils_paths
 
-debug('Create webpack compiler.')
-const compiler = webpack(webpackConfig)
-
-compiler.run((err, stats) => {
-  const jsonStats = stats.toJson()
-
-  debug('Webpack compile completed.')
-  console.log(stats.toString(config.compiler_stats))
-
-  if (err) {
-    debug('Webpack compiler encountered a fatal error.', err)
-    process.exit(1)
-  } else if (jsonStats.errors.length > 0) {
-    debug('Webpack compiler encountered errors.')
-    console.log(jsonStats.errors.join('\n'))
-    process.exit(1)
-  } else if (jsonStats.warnings.length > 0) {
-    debug('Webpack compiler encountered warnings.')
-
-    if (config.compiler_fail_on_warning) {
+;(async function () {
+  try {
+    debug('Run compiler')
+    const warnings = await webpackCompiler(webpackConfig)
+    if (warnings.length && config.compiler_fail_on_warning) {
+      debug('Config set to fail on warning, exiting with status code "1".')
       process.exit(1)
     }
-  } else {
-    debug('No errors or warnings encountered.')
+    debug('Copy static assets to dist folder.')
+    fs.copySync(paths.client('static'), paths.dist())
+  } catch (errors) {
+    process.exit(1)
   }
-
-  debug('Copy static assets to dist folder.')
-  fs.copySync(paths.client('static'), paths.dist())
-})
+})()
