86c8b82a3d90f26869f478de44a383aec0446d4a
David Zukowski <zukoweb@gmail.com>
2016-02-15 13:04:47 -0500

    chore(compile): remove debug log statement

--- bin/compile.js
+++ bin/compile.js
@@ -11,7 +11,6 @@ const paths = config.utils_paths
   try {
     debug('Run compiler')
     const stats = await webpackCompiler(webpackConfig)
-    console.log('stats', stats)
     if (stats.warnings.length && config.compiler_fail_on_warning) {
       debug('Config set to fail on warning, exiting with status code "1".')
       process.exit(1)
