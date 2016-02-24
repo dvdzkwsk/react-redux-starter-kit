0f7dae4851af509c57bfefbb38d53a20bfae0fe4
Christian Sakai <christianmsakai@gmail.com>
2016-02-24 14:44:11 -0500

    preparation for other tests in user.integration.js

--- server/api/user/user.integration.js
+++ server/api/user/user.integration.js
@@ -25,7 +25,34 @@ describe('User API:', function() {
     return User.remove();
   });
 
-  describe('GET /', function() {
+  xdescribe('GET /api/users', function() {
+    let token;
+
+    before(function(done) {
+      request(app)
+        .post('/auth/local')
+        .send({
+          email: 'admin@admin.com',
+          password: 'admin',
+          role: 'admin'
+        })
+        .expect(200)
+        .expecT('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+
+          token = res.body.token;
+          done();
+        });
+    });
+
+    it('should give admin access to view all users', function() {
+    });
+
+    it('should respond with 403 if not logged in as admin', function() {
+    });
 
   });
 
@@ -74,4 +101,99 @@ describe('User API:', function() {
         .end(done);
     });
   });
+
+  xdescribe('GET /api/users/:id', function() {
+    let token;
+
+    before(function(done) {
+      request(app)
+        .post('/auth/local')
+        .send({
+          email: 'test@example.com',
+          password: 'password'
+        })
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+
+          token = res.body.token;
+          done();
+        });
+    });
+
+    it('should respond with a user profile when authenticated', function(done) {
+      request(app)
+        .get('/api/users/me')
+        .set('authorization', 'Bearer ' + token)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          
+          expect(res.body._id.toString()).to.equal(user._id.toString());
+          done();
+        });
+    });
+
+    it('should respond with a 401 when not authenticated', function(done) {
+      request(app)
+        .get('/api/users/me')
+        .expect(401)
+        .end(done);
+    });
+  });
+
+  xdescribe('POST /api/users', function() {
+
+    it('should create a user locally', function() {
+    });
+
+  });
+
+  xdescribe('PUT /api/users/me/password', function() {
+
+    it('should be able to change own password', function() {
+    });
+
+    it('should respond with 403 if not authenticated', function() {
+    });
+
+  });
+
+  xdescribe('DELETE /api/users/:id', function() {
+    let token;
+
+    before(function(done) {
+      request(app)
+        .post('/auth/local')
+        .send({
+          email: 'admin@admin.com',
+          password: 'admin',
+          role: 'admin'
+        })
+        .expect(200)
+        .expecT('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+
+          token = res.body.token;
+          done();
+        });
+    });
+
+    it('should give admin access to delete a user', function() {
+    });
+
+    it('should respond with 403 if not logged in as admin', function() {
+    });
+
+  });
+
 });
