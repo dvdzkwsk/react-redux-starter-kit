12676f6f20116e167725016452da01dce86f6e5e
Curtis M. Humphrey, Ph.D <curtis@dataquest.io>
2016-02-26 13:44:57 -0600

    fix(chai-enzyme): fix issues when using cheerio when chai, add tests to confirm

--- tests/framework.spec.js
+++ tests/framework.spec.js
@@ -1,4 +1,17 @@
 import assert from 'assert'
+import React from 'react'
+import {mount, render, shallow} from 'enzyme'
+
+class Fixture extends React.Component {
+  render () {
+    return (
+      <div>
+        <input id='checked' defaultChecked />
+        <input id='not' defaultChecked={false} />
+      </div>
+    )
+  }
+}
 
 describe('(Framework) Karma Plugins', function () {
   it('Should expose "expect" globally.', function () {
@@ -17,5 +30,16 @@ describe('(Framework) Karma Plugins', function () {
       expect(pass).to.be.fulfilled,
       expect(fail).to.not.be.fulfilled
     ])
+  })
+
+  it('should have chai-enzyme working', function() {
+    let wrapper = shallow(<Fixture />)
+    expect(wrapper.find('#checked')).to.be.checked()
+
+    wrapper = mount(<Fixture />)
+    expect(wrapper.find('#checked')).to.be.checked()
+
+    wrapper = render(<Fixture />)
+    expect(wrapper.find('#checked')).to.be.checked()
   })
 })
