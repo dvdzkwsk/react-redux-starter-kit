118eca1bf2ed8533f70adf570bdc66ecebaf2ec3
Kevin Chiu <kevin.gc@gmail.com>
2016-03-01 11:56:44 -0800

    Revert "fix(webpack) Altered config to fix generated resource links"
    This reverts commit 18214bbb7922aa3469c98b6a53c75abb32496c4a.

--- config/_production.js
+++ config/_production.js
@@ -1,6 +1,5 @@
 /* eslint key-spacing:0 */
 export default () => ({
-  compiler_public_path: '/',
   compiler_fail_on_warning : false,
   compiler_hash_type       : 'chunkhash',
   compiler_devtool         : null,
