18214bbb7922aa3469c98b6a53c75abb32496c4a
Kevin Chiu <kevin.gc@gmail.com>
2016-02-29 20:24:54 -0800

    fix(webpack) Altered config to fix generated resource links

--- config/_production.js
+++ config/_production.js
@@ -1,5 +1,6 @@
 /* eslint key-spacing:0 */
 export default () => ({
+  compiler_public_path: '/',
   compiler_fail_on_warning : false,
   compiler_hash_type       : 'chunkhash',
   compiler_devtool         : null,
