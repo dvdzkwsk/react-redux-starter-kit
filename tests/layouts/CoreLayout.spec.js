f01d658de9b0275aa5c22d2ba0ebdf9fe558d8d7
Nathaniel Schweinberg <nathaniel@fightthecurrent.org>
2016-03-14 13:45:04 -0700

    reverted filenames to thing/NameThing/NameThing instead of thing/Name/NameThing

--- tests/layouts/CoreLayout.spec.js
+++ tests/layouts/CoreLayout.spec.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import TestUtils from 'react-addons-test-utils'
-import CoreLayout from 'layouts/Core/CoreLayout'
+import CoreLayout from 'layouts/CoreLayout/CoreLayout'
 
 function shallowRender (component) {
   const renderer = TestUtils.createRenderer()
--- tests/views/HomeView.spec.js
+++ tests/views/HomeView.spec.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import TestUtils from 'react-addons-test-utils'
 import { bindActionCreators } from 'redux'
-import { HomeView } from 'views/Home/HomeView'
+import { HomeView } from 'views/HomeView/HomeView'
 import { mount } from 'enzyme'
 
 function shallowRender (component) {
