dcbea851e6ea29926e976f09a460b95c85aed9b3
David Zukowski <zukoweb@gmail.com>
2016-03-16 23:05:09 -0500

    fix(css-modules): disable conflicting cssnano options

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -234,6 +234,9 @@ webpackConfig.postcss = [
     discardComments: {
       removeAll: true
     },
+    discardUnused: false,
+    mergeIdents: false,
+    reduceIdents: false,
     safe: true,
     sourcemap: true
   })
