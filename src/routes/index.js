714ec7e573236f23177bf0ad07a779ab53087107
Justin Greenberg <me@justingreenberg.com>
2016-04-26 16:39:18 -0400

    feat(routes): update routes and docs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,39 @@
 // We only need to import the modules necessary for initial render
 import CoreLayout from '../layouts/CoreLayout/CoreLayout'
 import Home from './Home'
+import CounterRoute from './Counter'
+import NotFoundRoute from './NotFound'
 
-export const createRoutes = (store) => {
-/*  Note: Instead of using JSX, we are using react-router PlainRoute,
-    a simple javascript object to provide route definitions.
-    When creating a new async route, pass the instantiated store!   */
+/*  Note: Instead of using JSX, we recommend using react-router
+    PlainRoute objects to build route definitions.   */
 
-  const routes = {
-    path: '/',
-    component: CoreLayout,
-    indexRoute: Home,
-    getChildRoutes (location, next) {
+export const createRoutes = (store) => ({
+  path: '/',
+  component: CoreLayout,
+  indexRoute: Home,
+  childRoutes: [
+    CounterRoute(store), // Provide instantiated store
+    NotFoundRoute
+  ]
+})
+
+/*  Note: childRoutes can be chunked or otherwise loaded programmatically
+    using getChildRoutes with the following signature:
+
+    getChildRoutes (location, cb) {
       require.ensure([], (require) => {
-        next(null, [
-          // Provide store for async reducers and middleware
+        cb(null, [
+          // Remove imports!
           require('./Counter').default(store),
           require('./NotFound').default
         ])
       })
     }
-  }
 
-  return routes
-}
+    However, this is not necessary for code-splitting! It simply provides
+    an API for async route definitions. Your code splitting should occur
+    inside the route `getComponent` function, since it is only invoked
+    when the route exists and matches.
+*/
 
 export default createRoutes
