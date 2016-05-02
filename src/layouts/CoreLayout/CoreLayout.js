6225e4f3e32782f21923c72d4db1019480a0a3eb
David Zukowski <zukoweb@gmail.com>
2016-05-01 20:20:24 -0400

    chore(lint): cleanup misc lint errors

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -5,7 +5,7 @@ import '../../styles/core.scss'
 
 export const CoreLayout = ({ children }) => (
   <div className='container text-center'>
-    <Header/>
+    <Header />
     <div className={classes.mainContainer}>
       {children}
     </div>
