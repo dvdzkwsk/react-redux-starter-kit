316fba4a14bd174788b6330b28341ce91b8e1bf6
Justin Greenberg <me@justingreenberg.com>
2016-09-20 22:04:49 -0400

    feat(core): init store with sync location reducer

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { browserHistory } from 'react-router'
 import createStore from './store/createStore'
 import AppContainer from './containers/AppContainer'
 
 // ========================================================
-// Store and History Instantiation
+// Store Instantiation
 // ========================================================
 const initialState = window.___INITIAL_STATE__
 const store = createStore(initialState)
@@ -19,11 +18,7 @@ let render = () => {
   const routes = require('./routes/index').default(store)
 
   ReactDOM.render(
-    <AppContainer
-      store={store}
-      history={browserHistory}
-      routes={routes}
-    />,
+    <AppContainer store={store} routes={routes} />,
     MOUNT_NODE
   )
 }
