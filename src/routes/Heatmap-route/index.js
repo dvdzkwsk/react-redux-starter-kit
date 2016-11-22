15f4abdc3c0f31b96750bd7b41104d9486372707
bin.yang <bin.yang@tarsocial.com>
2016-11-22 09:32:41 +0800

    temp

--- src/routes/Heatmap-route/index.js
+++ src/routes/Heatmap-route/index.js
@@ -1,14 +1,32 @@
-import CoreLayout from './layouts/CoreLayout'
-import Clickmap from './Clickmap'
-import Textmap from './Textmap'
+import HeatmapRoute from './View'
 
-export const createRoutes = (store) => ({
-  path        : '/heatmap',
-  component   : CoreLayout,
-  indexRoute  : Textmap,
-  childRoutes : [
-    Clickmap
-  ]
+// Sync route definition
+export default {
+  component : HeatmapRoute
+}
+
+
+import { injectReducer } from '../../store/reducers'
+
+export default (store) => ({
+  path : 'counter',
+  /*  Async getComponent is only invoked when route matches   */
+  getComponent (nextState, cb) {
+    /*  Webpack - use 'require.ensure' to create a split point
+        and embed an async module loader (jsonp) when bundling   */
+    require.ensure([], (require) => {
+      /*  Webpack - use require callback to define
+          dependencies for bundling   */
+      const Counter = require('./containers/CounterContainer').default
+      const reducer = require('./modules/counter').default
+
+      /*  Add the reducer to the store on key 'counter'  */
+      injectReducer(store, { key: 'counter', reducer })
+
+      /*  Return getComponent   */
+      cb(null, Counter)
+
+    /* Webpack named bundle   */
+    }, 'counter')
+  }
 })
-
-export default createRoutes
