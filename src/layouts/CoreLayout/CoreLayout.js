c361bc24460fea7b68439a7586051d99715a8049
Alexander Efimov <alexandr.efimov@gmail.com>
2016-12-10 19:59:20 +0500

    A little more beautiful

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from '../../components/Header'
+import Header from 'components/Header'
 import './CoreLayout.scss'
-import '../../styles/core.scss'
+import 'styles/core.scss'
 
 export const CoreLayout = ({ children }) => (
   <div className='container text-center'>
