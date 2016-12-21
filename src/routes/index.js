ca2e83fbb70fc4218e5a7ee8b42d5133704335f6
jakehm <jacob.harrowmortelliti@maine.edu>
2016-12-20 23:21:36 -0500

    refactor(routes): import correct CoreLayout file (#1115)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 // We only need to import the modules necessary for initial render
-import CoreLayout from '../layouts/CoreLayout/CoreLayout'
+import CoreLayout from '../layouts/CoreLayout'
 import Home from './Home'
 import CounterRoute from './Counter'
 
