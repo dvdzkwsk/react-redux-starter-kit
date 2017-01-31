ec09c91e0bb0232a5ee879f6e8e8292a0314f2bc
Ramon Boss <ramon.boss@outlook.com>
2017-01-31 12:04:08 +0100

    switched from jQuery to axios (promise based)

--- src/routes/Register/modules/register.js
+++ src/routes/Register/modules/register.js
@@ -1,4 +1,4 @@
-import jQuery from 'jquery'
+import axios from 'axios'
 
 // ------------------------------------
 // Constants
@@ -7,25 +7,39 @@ export const SUBMIT_REGISTER_FORM = 'SUBMIT_REGISTER_FORM'
 export const SUBMIT_REGISTER_FORM_PENDING = `${SUBMIT_REGISTER_FORM}_PENDING`
 export const SUBMIT_REGISTER_FORM_FULFILLED = `${SUBMIT_REGISTER_FORM}_FULFILLED`
 export const SUBMIT_REGISTER_FORM_REJECTED = `${SUBMIT_REGISTER_FORM}_REJECTED`
-
+// TODO https://github.com/rstacruz/nprogress
 // ------------------------------------
 // Actions
 // ------------------------------------
 export const postForm = () => {
   return (dispatch, getState) => {
     const data = JSON.stringify(getState().form.registerForm.values)
+
     dispatch({
       type: SUBMIT_REGISTER_FORM,
-      payload: new Promise(function(resolve, reject) {
-        jQuery.post({
-          url: 'http://127.0.0.1:3001',
-          contentType: 'application/json',
-          data,
-          success: resolve,
-          error: reject
-        })
-      })
+      payload: axios.post(
+        'url',
+        data,
+        { headers: {'Content-Type': 'application/json'} }
+      )
     })
+
+    // dispatch({
+    //   type: SUBMIT_REGISTER_FORM,
+    //   payload: new Promise((resolve, reject) => {
+    //     jQuery.post({
+    //       url: 'http://127.0.0.1:3001',
+    //       contentType: 'application/json',
+    //       data
+    //     }).then((data, textStatus, jqXHR) => {
+    //       delete jqXHR.then; // treat xhr as a non-promise
+    //       resolve(jqXHR);
+    //     }).fail((jqXHR, textStatus, errorThrown) => {
+    //       delete jqXHR.then; // treat xhr as a non-promise
+    //       reject(jqXHR);
+    //     })
+    //   })
+    // })
   }
 }
 
