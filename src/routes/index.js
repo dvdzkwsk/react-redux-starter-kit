e2d650c98a3345a999f3aeef6914a5765e3de475
Ramon Boss <ramon.boss@outlook.com>
2017-02-01 13:02:02 +0100

    added 404 page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import CoreLayout from '../layouts/CoreLayout'
 import Home from './Home'
 import CounterRoute from './Counter'
 import RegisterRoute from './Register'
+import NotFoundRoute from './NotFound'
 
 /*  Note: Instead of using JSX, we recommend using react-router
     PlainRoute objects to build route definitions.   */
@@ -13,7 +14,8 @@ export const createRoutes = (store) => ({
   indexRoute  : Home,
   childRoutes : [
     CounterRoute(store),
-    RegisterRoute(store)
+    RegisterRoute(store),
+    NotFoundRoute
   ]
 })
 
