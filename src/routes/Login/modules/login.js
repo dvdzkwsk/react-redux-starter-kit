99c56e6e70e30281d55ee601e894030366560def
Ramon Boss <ramon.boss@outlook.com>
2017-02-02 18:45:47 +0100

    dispatch user and auth action

--- src/routes/Login/modules/login.js
+++ src/routes/Login/modules/login.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { browserHistory } from 'react-router'
 import Cookies from 'js-cookie'
+import { AUTH_SUCCESS } from '../../../store/rootReducers/auth'
+import { FETCHED_USER_ID } from '../../../store/rootReducers/user'
 
 // ------------------------------------
 // Constants
@@ -10,19 +12,36 @@ export const SUBMIT_LOGIN_FORM_PENDING = `${SUBMIT_LOGIN_FORM}_PENDING`
 export const SUBMIT_LOGIN_FORM_FULFILLED = `${SUBMIT_LOGIN_FORM}_FULFILLED`
 export const SUBMIT_LOGIN_FORM_REJECTED = `${SUBMIT_LOGIN_FORM}_REJECTED`
 
+export const authSuccess = (authToken) => {
+  return {
+    type: AUTH_SUCCESS,
+    payload: { authToken }
+  }
+}
+
+export const fetchedUserId = (id) => {
+  return {
+    type: FETCHED_USER_ID,
+    payload: { id }
+  }
+}
+
 // ------------------------------------
 // Actions
 // ------------------------------------
 export const login = () => {
   return (dispatch, getState) => {
-    const data = JSON.stringify(getState().form.loginForm.values)
-    // TODO set TTL with rememberMe
+    let formData = getState().form.loginForm.values
+
+    if(!formData.rememberMe) formData.ttl = 10800 // set ttl to 3h if not remember me
+
+    formData = JSON.stringify(formData)
 
     dispatch({
       type: SUBMIT_LOGIN_FORM,
       payload: axios.post(
         'Users/login',
-        data,
+        formData
       ).then((response) => {
         const authToken = response.data.id
 
@@ -31,6 +50,9 @@ export const login = () => {
           expires: response.data.ttl / 60 / 60 / 24 // seconds to days
         })
 
+        dispatch(authSuccess(authToken))
+        dispatch(fetchedUserId(response.data.userId))
+
         browserHistory.push('/semesters')
       })
     })
@@ -38,7 +60,9 @@ export const login = () => {
 }
 
 export const actions = {
-  login
+  login,
+  authSuccess,
+  fetchedUserId
 }
 
 const LOGIN_ACTION_HANDLERS = {
