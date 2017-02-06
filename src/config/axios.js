698bcefbad7a589b6520db78ed29efd76a13de44
Ramon Boss <ramon.boss@outlook.com>
2017-02-06 10:31:25 +0100

    application json for put

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,4 +3,5 @@ import axios from 'axios'
 export const configureAxios = () => {
   axios.defaults.baseURL = 'http://localhost:3001/api'
   axios.defaults.headers.post['Content-Type'] = 'application/json'
+  axios.defaults.headers.put['Content-Type'] = 'application/json'
 }
