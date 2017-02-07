d279e96920aadcdfc12c002a0d1a26b99fe42663
Ramon Boss <ramon.boss@outlook.com>
2017-02-07 21:44:10 +0100

    error handling

--- src/store/rootReducers/index.js
+++ src/store/rootReducers/index.js
@@ -1,10 +1,12 @@
 import locationReducer from './location'
+import errorReducer from './error'
 import authReducer from './auth'
 import userReducer from './user'
 import { reducer as formReducer } from 'redux-form'
 
 export default {
   location: locationReducer,
+  errors: errorReducer,
   auth: authReducer,
   user: userReducer,
   form: formReducer
