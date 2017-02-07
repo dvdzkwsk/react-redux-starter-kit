c8ea05813a5c906c0d71430d1d4e30f9dde3effb
Ramon Boss <ramon.boss@outlook.com>
2017-02-07 22:27:23 +0100

    replaced by react bootstrap

--- src/routes/Semesters/components/Semesters.js
+++ src/routes/Semesters/components/Semesters.js
@@ -1,19 +1,17 @@
 import React from 'react'
-import { browserHistory } from 'react-router'
+import { ListGroup, ListGroupItem } from 'react-bootstrap'
 import { mode } from '../modules/semesters'
 
 export const Semesters = (props) => (
   <div>
-    <div className='list-group'>
+    <ListGroup>
       { props.semesters.map(semester => (
-        <button className='list-group-item' key={semester.id}
-          onClick={() => props.semesterClick(semester.id)}
-        >
+        <ListGroupItem key={semester.id} onClick={() => props.semesterClick(semester.id)}>
           <span className='badge'>14</span>
           { semester.name }
-        </button>
+        </ListGroupItem>
       )) }
-    </div>
+    </ListGroup>
     <button className='btn btn-default' onClick={() => {props.modeButtonClick(mode.add); props.semesterClick(null)}}>new</button>
     <button className={props.mode === mode.edit ? 'btn btn-primary' : 'btn btn-default'} onClick={() => props.modeButtonClick(mode.edit)}>edit</button>
     <button className={props.mode === mode.info ? 'btn btn-primary' : 'btn btn-default'} onClick={() => props.modeButtonClick(mode.info)}>info</button>
