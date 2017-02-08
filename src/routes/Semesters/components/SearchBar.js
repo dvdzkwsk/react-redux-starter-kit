70c64fe965d719659f5b9482bfee6fcdd2226d4a
Ramon Boss <ramon.boss@outlook.com>
2017-02-08 11:03:17 +0100

    styled bar

--- src/routes/Semesters/components/SearchBar.js
+++ src/routes/Semesters/components/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 export const SearchBar = (props) => (
-  <Field component='input' name='searchSemesterField' type='text' placeholder='search semesters...' />
+  <Field component='input' name='searchSemesterField' type='text' placeholder='search semesters...' className='form-control' />
 )
 
 export default reduxForm({
