38d64be3b7ff7648b830b82583f2bd593ec7fbf2
Ramon Boss <ramon.boss@outlook.com>
2017-02-08 10:06:49 +0100

    infoMode as const

--- src/routes/Semester/components/Semester.js
+++ src/routes/Semester/components/Semester.js
@@ -2,18 +2,22 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { mode } from '../../Semesters/modules/semesters'
+import { browserHistory } from 'react-router'
 
-let Semester = (props) => (
-  <form id='semesterForm'>
-    <fieldset disabled={props.mode === mode.info}>
+let Semester = (props) => {
+  const infoMode = props.mode === mode.info
+  return (
+    <form id='semesterForm'>
       {/* TODO replace type with hidden */}
-      <Field component='input' name='id' type='number' placeholder='ID' className='form-control' disabled='true' />
-      <Field component='input' name='name' type='text' placeholder='name' className='form-control' />
-      <button type='button' onClick={props.handleSubmit} disabled={props.pristine || props.submitting} className='btn btn-primary'>Submit</button>
-      <button type='button' disabled={props.pristine || props.submitting} onClick={props.reset} className='btn btn-default'>reset</button>
-    </fieldset>
-  </form>
-)
+      <fieldset disabled={infoMode}>
+        <Field component='input' name='id' type='number' placeholder='ID' className='form-control' disabled='true'/>
+        <Field component='input' name='name' type='text' placeholder='name' className='form-control'/>
+      </fieldset>
+      { !infoMode ? (<button type='button' onClick={props.handleSubmit} disabled={props.pristine || props.submitting} className='btn btn-primary'>Submit</button>) : ''}
+      <button type='button' onClick={browserHistory.goBack} className='btn btn-default'>cancel</button>
+    </form>
+  );
+}
 
 Semester = reduxForm({
   form: 'semesterForm'
