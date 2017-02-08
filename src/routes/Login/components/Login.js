05316682bb1303d868b3111511cf0863a701cb27
Ramon Boss <ramon.boss@outlook.com>
2017-02-08 21:16:14 +0100

    styled

--- src/routes/Login/components/Login.js
+++ src/routes/Login/components/Login.js
@@ -1,25 +1,36 @@
 import React from 'react'
-import { Form, InputGroup, Button } from 'react-bootstrap'
+import { Form, FormGroup, Col, ControlLabel, InputGroup, Button } from 'react-bootstrap'
 import FontAwesome from 'react-fontawesome'
 import { Field, reduxForm } from 'redux-form'
 
 export const Login = (props) => (
-  <Form>
-    <InputGroup>
-      <InputGroup.Addon><FontAwesome name='at' /></InputGroup.Addon>
-      <Field component='input' name='email' type='text' placeholder='e-mail' className='form-control' />
-    </InputGroup>
-    <InputGroup>
-      <InputGroup.Addon><FontAwesome name='key' /></InputGroup.Addon>
-      <Field component='input' name='password' type='password' placeholder='password' className='form-control' />
-    </InputGroup>
-    <div className='checkbox'>
-      <label>
-        <Field component='input' id='rememberMe' name='rememberMe' type='checkbox' label='remember me' /> remember me
-      </label>
-    </div>
-    <Button type='button' bsStyle='primary' onClick={props.handleSubmit} disabled={props.pristine || props.submitting}>Submit</Button>
-    <Button type='button' disabled={props.pristine || props.submitting} onClick={props.reset}>Clear Values</Button>
+  <Form horizontal>
+    <FormGroup>
+      <Col componentClass={ControlLabel} sm={2}>Email</Col>
+      <Col sm={10}>
+        <Field component='input' name='email' type='text' placeholder='e-mail' className='form-control' />
+      </Col>
+    </FormGroup>
+    <FormGroup>
+      <Col componentClass={ControlLabel} sm={2}>Password</Col>
+      <Col sm={10}>
+        <Field component='input' name='password' type='password' placeholder='password' className='form-control' />
+      </Col>
+    </FormGroup>
+    <FormGroup>
+      <Col smOffset={2} sm={10}>
+        <div className='checkbox'>
+          <label>
+            <Field component='input' id='rememberMe' name='rememberMe' type='checkbox' label='remember me' /> remember me
+          </label>
+        </div>
+      </Col>
+    </FormGroup>
+    <FormGroup>
+      <Col smOffset={2} sm={10}>
+        <Button type='button' onClick={props.handleSubmit} disabled={props.pristine || props.submitting}>Sign in</Button>
+      </Col>
+    </FormGroup>
   </Form>
 )
 
