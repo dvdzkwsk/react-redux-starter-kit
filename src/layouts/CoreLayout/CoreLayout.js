03ba4894fe8dc5d4e19383e8ac03f017aa4c2edb
Ramon Boss <ramon.boss@outlook.com>
2017-02-08 19:43:30 +0100

    removed text-center

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -5,7 +5,7 @@ import './CoreLayout.scss'
 import '../../styles/core.scss'
 
 export const CoreLayout = ({ children }) => (
-  <div className='container-fluid text-center'>
+  <div className='container-fluid'>
     <Error />
     <Header />
     <div className='core-layout__viewport container'>
