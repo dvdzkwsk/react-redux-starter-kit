237b26564c58045ffbb6984302a735de08df6189
Ramon Boss <ramon.boss@outlook.com>
2017-02-08 19:43:49 +0100

    added label to fields

--- src/routes/Register/components/Register.js
+++ src/routes/Register/components/Register.js
@@ -1,21 +1,32 @@
 import React from 'react'
-import { Form, InputGroup, Button } from 'react-bootstrap'
-import FontAwesome from 'react-fontawesome'
+import { FormGroup, ControlLabel, Button } from 'react-bootstrap'
 import { Field, reduxForm } from 'redux-form'
 
 export const Register = (props) => (
-  <Form>
-    <InputGroup>
-      <InputGroup.Addon><FontAwesome name='address-book' /></InputGroup.Addon>
+  <form>
+    <FormGroup>
+      <ControlLabel>First name</ControlLabel>
+      <Field component='input' name='firstName' type='text' placeholder='first name' className='form-control' />
+    </FormGroup>
+    <FormGroup>
+      <ControlLabel>Surname</ControlLabel>
+      <Field component='input' name='surname' type='text' placeholder='surname' className='form-control' />
+    </FormGroup>
+    <FormGroup>
+      <ControlLabel>Email</ControlLabel>
       <Field component='input' name='email' type='text' placeholder='e-mail' className='form-control' />
-    </InputGroup>
-    <InputGroup>
-      <InputGroup.Addon><FontAwesome name='key' /></InputGroup.Addon>
+    </FormGroup>
+    <FormGroup>
+      <ControlLabel>Password</ControlLabel>
       <Field component='input' name='password' type='password' placeholder='password' className='form-control' />
-    </InputGroup>
+    </FormGroup>
+    <FormGroup>
+      <ControlLabel>Password repeat</ControlLabel>
+      <Field component='input' name='passwordRepeat' type='password' placeholder='password-repeat' className='form-control' />
+    </FormGroup>
     <Button type='button' bsStyle='primary' onClick={props.handleSubmit} disabled={props.pristine || props.submitting}>Submit</Button>
     <Button type='button' disabled={props.pristine || props.submitting} onClick={props.reset}>Clear Values</Button>
-  </Form>
+  </form>
 )
 
 Register.propTypes = {
