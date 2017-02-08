2b65b105c8f163811eca5edb78a73f0c49ea01a6
Ramon Boss <ramon.boss@outlook.com>
2017-02-08 12:45:05 +0100

    styled form

--- src/routes/Register/components/Register.js
+++ src/routes/Register/components/Register.js
@@ -1,23 +1,21 @@
 import React from 'react'
+import { Form, InputGroup, Button } from 'react-bootstrap'
+import FontAwesome from 'react-fontawesome'
 import { Field, reduxForm } from 'redux-form'
 
-const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
-  <div>
-    <label>{label}</label>
-    <div>
-      <input {...input} placeholder={label} type={type} />
-      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
-    </div>
-  </div>
-)
-
 export const Register = (props) => (
-  <form id='registerForm'>
-    <Field component={renderField} name='email' type='text' label='e-mail' />
-    <Field component={renderField} name='password' type='password' label='password' />
-    <button type='button' onClick={props.handleSubmit} disabled={props.pristine || props.submitting}>Submit</button>
-    <button type='button' disabled={props.pristine || props.submitting} onClick={props.reset}>Clear Values</button>
-  </form>
+  <Form>
+    <InputGroup>
+      <InputGroup.Addon><FontAwesome name='address-book' /></InputGroup.Addon>
+      <Field component='input' name='email' type='text' placeholder='e-mail' className='form-control' />
+    </InputGroup>
+    <InputGroup>
+      <InputGroup.Addon><FontAwesome name='key' /></InputGroup.Addon>
+      <Field component='input' name='password' type='password' placeholder='password' className='form-control' />
+    </InputGroup>
+    <Button type='button' bsStyle='primary' onClick={props.handleSubmit} disabled={props.pristine || props.submitting}>Submit</Button>
+    <Button type='button' disabled={props.pristine || props.submitting} onClick={props.reset}>Clear Values</Button>
+  </Form>
 )
 
 Register.propTypes = {
