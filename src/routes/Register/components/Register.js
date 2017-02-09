c087e52f3ee0df0ac98367deaf8c34e6addc3081
Ramon Boss <ramon.boss@outlook.com>
2017-02-09 15:54:30 +0100

    removed unused import

--- src/routes/Register/components/Register.js
+++ src/routes/Register/components/Register.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
 import { Field, Form, reduxForm } from 'redux-form'
 import { TextField } from 'redux-form-material-ui'
 import FlatButton from 'material-ui/FlatButton';
