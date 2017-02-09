298c08e4be9ad78d0e7d520159169c08c57b061b
Sam Coope <sam.j.coope@gmail.com>
2017-02-09 14:12:40 +0000

    Change title - no tests

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.scss'
 
 export const Header = () => (
   <div>
-    <h1>React Redux Starter Kit</h1>
+    <h1>Greetings Robert</h1>
     <IndexLink to='/' activeClassName='route--active'>
       Home
     </IndexLink>
