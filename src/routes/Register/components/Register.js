9c840ba86613402cbaa9d00f6b129d63c205c235
Ramon Boss <ramon.boss@outlook.com>
2017-02-09 15:53:00 +0100

    styled with Material UI

--- src/routes/Register/components/Register.js
+++ src/routes/Register/components/Register.js
@@ -1,32 +1,19 @@
 import React from 'react'
-import { FormGroup, ControlLabel, Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import { Field, Form, reduxForm } from 'redux-form'
+import { TextField } from 'redux-form-material-ui'
+import FlatButton from 'material-ui/FlatButton';
 import { registerSucceed, registerFailed } from '../modules/register'
 
 export const Register = (props) => (
   <Form onSubmit={props.handleSubmit(props.register)} form='form'>
-    <FormGroup>
-      <ControlLabel>First Name</ControlLabel>
-      <Field component='input' name='firstName' type='text' placeholder='first name' className='form-control' />
-    </FormGroup>
-    <FormGroup>
-      <ControlLabel>Surname</ControlLabel>
-      <Field component='input' name='surname' type='text' placeholder='surname' className='form-control' />
-    </FormGroup>
-    <FormGroup>
-      <ControlLabel>Email</ControlLabel>
-      <Field component='input' name='email' type='text' placeholder='e-mail' className='form-control' />
-    </FormGroup>
-    <FormGroup>
-      <ControlLabel>Password</ControlLabel>
-      <Field component='input' name='password' type='password' placeholder='password' className='form-control' />
-    </FormGroup>
-    <FormGroup>
-      <ControlLabel>Password repeat</ControlLabel>
-      <Field component='input' name='passwordRepeat' type='password' placeholder='password-repeat' className='form-control' />
-    </FormGroup>
-    <Button type='submit' bsStyle='primary' disabled={props.pristine || props.submitting}>Submit</Button>
-    <Button type='button' disabled={props.pristine || props.submitting} onClick={props.reset}>Clear Values</Button>
+    <Field name='firstName' component={TextField} floatingLabelText='first name' />
+    <Field name='surname' component={TextField} floatingLabelText='surname' />
+    <Field name='email' component={TextField} floatingLabelText='e-mail' />
+    <Field name='password' component={TextField} type='password' floatingLabelText='password' />
+    <Field name='passwordRepeat' component={TextField} type='password' floatingLabelText='password-repeat' />
+    <FlatButton type='submit' disabled={props.pristine || props.submitting}>Submit</FlatButton>
+    <FlatButton disabled={props.pristine || props.submitting} onClick={props.reset}>Clear Values</FlatButton>
   </Form>
 )
 
