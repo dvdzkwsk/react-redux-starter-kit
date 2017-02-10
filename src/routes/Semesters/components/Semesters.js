22e90fbd1f2ca5fbd5d3ac66776de719dbcca16f
Ramon Boss <ramon.boss@outlook.com>
2017-02-10 12:38:48 +0100

    removed unuser old taskbar

--- src/routes/Semesters/components/Semesters.js
+++ src/routes/Semesters/components/Semesters.js
@@ -1,16 +1,7 @@
 import React from 'react'
-import FontAwesome from 'react-fontawesome'
 import {List, ListItem} from 'material-ui/List'
-import FlatButton from 'material-ui/FlatButton'
-import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation'
-import { mode } from '../modules/semestersMainView'
 import SearchBar from '../containers/SearchBarContainer'
 import ActionBar from '../containers/SemestersActionBarContainer'
-import Add from 'material-ui/svg-icons/content/add'
-import Edit from 'material-ui/svg-icons/image/edit'
-import Info from 'material-ui/svg-icons/action/info'
-import Delete from 'material-ui/svg-icons/action/delete'
-import Search from 'material-ui/svg-icons/action/search'
 
 export const Semesters = (props) => (
   <div>
@@ -23,11 +14,6 @@ export const Semesters = (props) => (
       </ListItem>
     )) }
     </List>
-    <FlatButton onClick={() => { props.modeButtonClick(mode.add); props.semesterClick(null) }}><FontAwesome name='plus' /></FlatButton>
-    <FlatButton {...props.mode === mode.edit ? { primary: true } : {}} ><FontAwesome name='edit' /></FlatButton>
-    <FlatButton {...props.mode === mode.info ? { primary: true } : {}}><FontAwesome name='info' /></FlatButton>
-    <FlatButton {...props.mode === mode.remove ? { primary: true } : {}}><FontAwesome name='remove' /></FlatButton>
-    <FlatButton {...props.showSearchBar ? { primary: true } : {}}><FontAwesome name='search' /></FlatButton>
     <ActionBar />
   </div>
 )
