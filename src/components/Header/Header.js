754820b074d65a8bc4c87aefb6fbacdad6b26897
Ramon Boss <ramon.boss@outlook.com>
2017-02-14 13:48:04 +0100

    use browserHistory.push() instead of direct links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { browserHistory } from 'react-router'
 import { Navbar, Nav, NavItem } from 'react-bootstrap'
 import FontAwesome from 'react-fontawesome'
 
@@ -8,7 +9,7 @@ export const Header = (props) => (
   <Navbar fluid>
     <Navbar.Header>
       <Navbar.Brand>
-        <Navbar.Link href='/'><FontAwesome name='home' /> GPA-App</Navbar.Link>
+        <Navbar.Link onClick={() => browserHistory.push('/')}><FontAwesome name='home' /> GPA-App</Navbar.Link>
       </Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
@@ -16,9 +17,9 @@ export const Header = (props) => (
      <Navbar.Collapse>
           <Nav>
             {props.user ? (<NavItem disabled>Signed in as: {props.user.firstName} {props.user.surname}</NavItem>) : ''}
-            <NavItem><FontAwesome name='user' /> Profile</NavItem>
-            <NavItem href='about'><FontAwesome name='question-circle' /> About</NavItem>
-            <NavItem><FontAwesome name='gear' /> Settings</NavItem>
+            <NavItem onClick={() => browserHistory.push('profile')}><FontAwesome name='user' /> Profile</NavItem>
+            <NavItem onClick={() => browserHistory.push('about')}><FontAwesome name='question-circle' /> About</NavItem>
+            <NavItem onClick={() => browserHistory.push('settings')}><FontAwesome name='gear' /> Settings</NavItem>
           </Nav>
           <Nav pullRight>
             <NavItem onClick={() => browserHistory.push('logout')}><FontAwesome name='sign-out' /> Sign out</NavItem>
