66a3f98ad7740321bbd8e6fc97f2e9d1fe7a68bc
Ramon Boss <ramon.boss@outlook.com>
2017-02-14 12:21:33 +0100

    added bootstrap navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,31 @@
 import React from 'react'
-import AppBar from 'material-ui/AppBar'
-import Menu from 'material-ui/svg-icons/navigation/menu'
+import { Navbar, Nav, NavItem } from 'react-bootstrap'
+import FontAwesome from 'react-fontawesome'
+
 import './Header.scss'
 
 export const Header = (props) => (
-  <AppBar
-    showMenuIconButton={false}
-    title=' GPA-App'
-    iconElementRight={<Menu />}
-    onRightIconButtonTouchTap={props.toggle}
-  />
+  <Navbar fluid>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <Navbar.Link href='/'><FontAwesome name='home' /> GPA-App</Navbar.Link>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    {props.loggedIn ? (
+     <Navbar.Collapse>
+          <Nav>
+            {props.user ? (<NavItem disabled>Signed in as: {props.user.firstName} {props.user.surname}</NavItem>) : ''}
+            <NavItem><FontAwesome name='user' /> Profile</NavItem>
+            <NavItem href='about'><FontAwesome name='question-circle' /> About</NavItem>
+            <NavItem><FontAwesome name='gear' /> Settings</NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavItem onClick={() => browserHistory.push('logout')}><FontAwesome name='sign-out' /> Sign out</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+    ) : ''}
+  </Navbar>
 )
 
 export default Header
