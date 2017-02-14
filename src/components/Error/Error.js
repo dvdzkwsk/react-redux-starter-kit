77f7eb51ff568ae3018428b2fbd63bcf283ba779
Ramon Boss <ramon.boss@outlook.com>
2017-02-14 12:45:32 +0100

    bootstrap alert for error messages

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import Snackbar from 'material-ui/Snackbar'
+import { Alert } from 'react-bootstrap'
 
 import './Error.scss'
 
 export const Error = (props) => (
-  <Snackbar open={props.errors.length > 0}
-    message={props.errors[0] ? props.errors[0].message : ''}
-    autoHideDuration={5000}
-    action='CLOSE'
-    onRequestClose={() => props.removeByMessage(props.errors[0].message)}
-    onActionTouchTap={() => props.removeByMessage(props.errors[0].message)}
-  />
+  <div id='alerts-queue'>
+    {props.errors.map((error, index) => (
+    <Alert key={index} bsStyle='danger' onDismiss={() => props.removeByMessage(error.message)}>
+      <p>{error.message}</p>
+    </Alert>
+    ))}
+  </div>
 )
 
 export default Error
