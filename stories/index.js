f8259ce613caa32ed779f94c5043fd67fe98af7c
Oliver Caine <olliecaine@gmail.com>
2017-05-14 14:32:30 +1000

    Adding in other components

--- stories/index.js
+++ stories/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import { storiesOf, action, linkTo } from '@kadira/storybook';
-import Header from '../src/components/Header';
+import React from 'react'
+import { storiesOf, action, linkTo } from '@kadira/storybook'
+import Counter from '../src/routes/Counter/components/Counter'
+import HomeView from '../src/routes/Home/components/HomeView'
+import Header from '../src/components/Header'
+
+storiesOf('Counter', module)
+    .add('Default', () => (
+        <Counter />
+    ));
 
 storiesOf('Header', module)
     .add('Default', () => (
         <Header />
+    ));
+
+storiesOf('HomeView', module)
+    .add('Default', () => (
+        <HomeView />
     ));
