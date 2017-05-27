5edacb4a06de27108c6dda679facc50ec060ea3f
Konstantinos Leimonis <kleimonis@gmail.com>
2017-05-28 01:37:10 +0300

    refactor(jest): update Counter route tests to jest

--- tests/routes/Counter/modules/counter.spec.js
+++ tests/routes/Counter/modules/counter.spec.js
@@ -7,45 +7,45 @@ import {
 
 describe('(Redux Module) Counter', () => {
   it('Should export a constant COUNTER_INCREMENT.', () => {
-    expect(COUNTER_INCREMENT).to.equal('COUNTER_INCREMENT')
+    expect(COUNTER_INCREMENT).toBe('COUNTER_INCREMENT')
   })
 
   describe('(Reducer)', () => {
     it('Should be a function.', () => {
-      expect(counterReducer).to.be.a('function')
+      expect(typeof counterReducer).toBe('function')
     })
 
     it('Should initialize with a state of 0 (Number).', () => {
-      expect(counterReducer(undefined, {})).to.equal(0)
+      expect(counterReducer(undefined, {})).toBe(0)
     })
 
     it('Should return the previous state if an action was not matched.', () => {
       let state = counterReducer(undefined, {})
-      expect(state).to.equal(0)
+      expect(state).toBe(0)
       state = counterReducer(state, { type: '@@@@@@@' })
-      expect(state).to.equal(0)
+      expect(state).toBe(0)
       state = counterReducer(state, increment(5))
-      expect(state).to.equal(5)
+      expect(state).toBe(5)
       state = counterReducer(state, { type: '@@@@@@@' })
-      expect(state).to.equal(5)
+      expect(state).toBe(5)
     })
   })
 
   describe('(Action Creator) increment', () => {
     it('Should be exported as a function.', () => {
-      expect(increment).to.be.a('function')
+      expect(typeof increment).toBe('function')
     })
 
     it('Should return an action with type "COUNTER_INCREMENT".', () => {
-      expect(increment()).to.have.property('type', COUNTER_INCREMENT)
+      expect(increment()).toHaveProperty('type', COUNTER_INCREMENT)
     })
 
     it('Should assign the first argument to the "payload" property.', () => {
-      expect(increment(5)).to.have.property('payload', 5)
+      expect(increment(5)).toHaveProperty('payload', 5)
     })
 
     it('Should default the "payload" property to 1 if not provided.', () => {
-      expect(increment()).to.have.property('payload', 1)
+      expect(increment()).toHaveProperty('payload', 1)
     })
   })
 
@@ -58,34 +58,36 @@ describe('(Redux Module) Counter', () => {
       _globalState = {
         counter : counterReducer(undefined, {})
       }
-      _dispatchSpy = sinon.spy((action) => {
+      _dispatchSpy = jest.fn((action) => {
         _globalState = {
           ..._globalState,
           counter : counterReducer(_globalState.counter, action)
         }
       })
-      _getStateSpy = sinon.spy(() => {
+      _getStateSpy = jest.fn(() => {
         return _globalState
       })
     })
 
     it('Should be exported as a function.', () => {
-      expect(doubleAsync).to.be.a('function')
+      expect(typeof doubleAsync).toBe('function')
     })
 
     it('Should return a function (is a thunk).', () => {
-      expect(doubleAsync()).to.be.a('function')
+      expect(typeof doubleAsync()).toBe('function')
     })
 
     it('Should return a promise from that thunk that gets fulfilled.', () => {
-      return doubleAsync()(_dispatchSpy, _getStateSpy).should.eventually.be.fulfilled
+      expect.assertions(1)
+      const doubleAsyncPromise = doubleAsync()(_dispatchSpy, _getStateSpy)
+      return expect(doubleAsyncPromise).resolves.toBe(undefined)
     })
 
     it('Should call dispatch and getState exactly once.', () => {
       return doubleAsync()(_dispatchSpy, _getStateSpy)
         .then(() => {
-          _dispatchSpy.should.have.been.calledOnce()
-          _getStateSpy.should.have.been.calledOnce()
+          expect(_dispatchSpy).toHaveBeenCalledTimes(1)
+          expect(_getStateSpy).toHaveBeenCalledTimes(1)
         })
     })
 
@@ -94,15 +96,15 @@ describe('(Redux Module) Counter', () => {
 
       return doubleAsync()(_dispatchSpy, _getStateSpy)
         .then(() => {
-          _dispatchSpy.should.have.been.calledOnce()
-          _getStateSpy.should.have.been.calledOnce()
-          expect(_globalState.counter).to.equal(4)
+          expect(_dispatchSpy).toHaveBeenCalledTimes(1)
+          expect(_getStateSpy).toHaveBeenCalledTimes(1)
+          expect(_globalState.counter).toBe(4)
           return doubleAsync()(_dispatchSpy, _getStateSpy)
         })
         .then(() => {
-          _dispatchSpy.should.have.been.calledTwice()
-          _getStateSpy.should.have.been.calledTwice()
-          expect(_globalState.counter).to.equal(8)
+          expect(_dispatchSpy).toHaveBeenCalledTimes(2)
+          expect(_getStateSpy).toHaveBeenCalledTimes(2)
+          expect(_globalState.counter).toBe(8)
         })
     })
   })
@@ -113,13 +115,13 @@ describe('(Redux Module) Counter', () => {
   describe('(Action Handler) COUNTER_INCREMENT', () => {
     it('Should increment the state by the action payload\'s "value" property.', () => {
       let state = counterReducer(undefined, {})
-      expect(state).to.equal(0)
+      expect(state).toBe(0)
       state = counterReducer(state, increment(1))
-      expect(state).to.equal(1)
+      expect(state).toBe(1)
       state = counterReducer(state, increment(2))
-      expect(state).to.equal(3)
+      expect(state).toBe(3)
       state = counterReducer(state, increment(-3))
-      expect(state).to.equal(0)
+      expect(state).toBe(0)
     })
   })
 })
