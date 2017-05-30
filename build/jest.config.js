6947713152c14ffe2b4fc24116f37b7ac6ff7f39
Konstantinos Leimonis <kleimonis@gmail.com>
2017-05-31 01:37:10 +0300

    refactor(jest): fix eslint errors

--- build/jest.config.js
+++ build/jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   'moduleDirectories': ['node_modules', 'src'],
   'moduleNameMapper': {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/__mocks__/fileMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|mp3)$': '<rootDir>/tests/__mocks__/fileMock.js',
     '\\.(css|less|scss)$': '<rootDir>/tests/__mocks__/styleMock.js'
   },
   'rootDir': project.basePath,
