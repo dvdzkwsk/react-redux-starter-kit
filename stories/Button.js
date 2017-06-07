09608f94c05d17ac26fbf23da1ae9fc636af8a53
Oliver Caine <olliecaine@gmail.com>
2017-06-08 07:53:08 +1000

    Installed non-Kadira version of React Storybook

---
+++ stories/Button.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const buttonStyles = {
+  border: '1px solid #eee',
+  borderRadius: 3,
+  backgroundColor: '#FFFFFF',
+  cursor: 'pointer',
+  fontSize: 15,
+  padding: '3px 10px',
+  margin: 10,
+};
+
+const Button = ({ children, onClick }) => (
+  <button style={buttonStyles} onClick={onClick}>
+    {children}
+  </button>
+);
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+export default Button;
