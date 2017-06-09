0aa0e3af1d3d6c04aa6a4e8bb30d847ce2ae47ce
Trevor Alexander <alexander.trevor@mail.com>
2017-06-09 09:28:07 -0700

    Removed unneeded dependencies
    Also removed deprecated env command for webpack 2

--- build/scripts/start.js
+++ build/scripts/start.js
@@ -1,6 +1,6 @@
-const logger = require('../lib/logger')
+const logger = require('../lib/logger');
 
-logger.info('Starting server...')
+logger.info('Starting server...');
 require('../../server/main').listen(3000, () => {
-  logger.success('Server is running at http://localhost:3000')
-})
+  logger.success('Server is running at http://localhost:3000');
+});
