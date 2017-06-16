decb51ef927ee1af761b1ee1fab9cc2092afbbcc
Sandeep Poman <sandeepp@nitorinfotech.com>
2017-06-16 10:00:02 +0530

    Supporting the route switch in time travel debugging

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { browserHistory, Router } from 'react-router'
 import { Provider } from 'react-redux'
 import PropTypes from 'prop-types'
+import { syncHistoryWithStore } from 'react-router-redux'
 
 class App extends React.Component {
   static propTypes = {
@@ -14,10 +15,11 @@ class App extends React.Component {
   }
 
   render () {
+    const history = syncHistoryWithStore(browserHistory, this.props.store)
     return (
       <Provider store={this.props.store}>
         <div style={{ height: '100%' }}>
-          <Router history={browserHistory} children={this.props.routes} />
+          <Router history={history} children={this.props.routes} />
         </div>
       </Provider>
     )
