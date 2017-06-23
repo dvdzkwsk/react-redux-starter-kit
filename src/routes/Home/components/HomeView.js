b39640f8cddfafd79ea7b68a5112658c26d2273b
Jason Sohl <jason@frozensoliddesigns.com>
2017-06-23 15:45:35 -0700

    working on sending and receiving

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
+import React, { Component } from 'react'
 import './HomeView.scss'
 
-export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img alt='This is a duck, because Redux!' className='duck' src={DuckImage} />
-  </div>
-)
+class HomeView extends Component {
+  sendLogin () {
+    fetch('http://localhost:3000/authenticate', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: document.getElementById('login-email').value,
+        password: document.getElementById('login-password').value,
+      })
+    }).then((response) => {
+      console.log(response)
+    })
+    .then((responseJson) => {
+      console.log(responseJson)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }
+  render () {
+    return (<div>
+      <input type='text' id='login-email' placeholder='email' />
+      <input type='password' id='login-password' placeholder='password' />
+      <button id='x-btn-send-login' onClick={this.sendLogin}>Login</button>
+    </div>)
+  }
+}
 
 export default HomeView
